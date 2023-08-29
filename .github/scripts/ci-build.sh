f767db7b9797bdf832a491296ae9476e87cf608d
Kunshan Wang <wks1986@gmail.com>
2023-08-29 17:36:51 +0800

    Workaround Ruby build in CI (#28)
    Sometimes the compiling of `encoding.c` started before `id.h` was
    generated, causing building to fail.

--- .github/scripts/ci-build.sh
+++ .github/scripts/ci-build.sh
@@ -19,6 +19,8 @@ pushd $BINDING_PATH
     esac
 popd
 
+NUM_OF_JOBS=2
+
 pushd $RUBY_PATH
 
     ./autogen.sh
@@ -29,12 +31,12 @@ pushd $RUBY_PATH
     case $DEBUG_LEVEL in
         debug)
             ../configure --with-mmtk-ruby=$BINDING_PATH --with-mmtk-ruby-debug --prefix=$RUBY_INSTALL_PATH --disable-install-doc cppflags='-g3 -O0 -DRUBY_DEBUG=1 -DRUBY_DEVEL -DUSE_RUBY_DEBUG_LOG=1'
-            make miniruby -j
+            make miniruby -j $NUM_OF_JOBS
             ;;
 
         release)
             ../configure --with-mmtk-ruby=$BINDING_PATH --prefix=$RUBY_INSTALL_PATH --disable-install-doc cppflags='-g3'
-            make install -j
+            make install -j $NUM_OF_JOBS
             ;;
 
         *)
--- .github/scripts/common.sh
+++ .github/scripts/common.sh
@@ -2,6 +2,4 @@ BINDING_PATH=$(realpath $(dirname "$0"))/../..
 RUBY_PATH=$BINDING_PATH/../ruby
 RUBY_BUILD_PATH=$RUBY_PATH/build
 RUBY_INSTALL_PATH=$RUBY_BUILD_PATH/install
-RUSTUP_TOOLCHAIN=stable
-# TODO: Switch to a fixed toolchain the next time we bump MSRV
-#RUSTUP_TOOLCHAIN=`cat $BINDING_PATH/mmtk/rust-toolchain`
+RUSTUP_TOOLCHAIN=`cat $BINDING_PATH/mmtk/rust-toolchain`
