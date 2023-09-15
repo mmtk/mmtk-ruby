5bd349c659e7e43a16b2d9ffa12804c1e4b1055b
Kunshan Wang <wks1986@gmail.com>
2023-09-15 17:26:39 +0800

    Run CI tests against a specified ruby revision

---
+++ .github/scripts/extract-ruby-revision.py
@@ -0,0 +1,27 @@
+import argparse
+import tomllib
+import os
+
+parser = argparse.ArgumentParser(
+        description='Extract the Ruby repo revision to test against',
+        )
+
+parser.add_argument('toml_path', help='Path to Cargo.toml')
+parser.add_argument('--github-output', help='Print in the format of GITHUB_OUTPUT')
+
+args = parser.parse_args()
+
+with open(args.toml_path, "rb") as f:
+    toml_data = tomllib.load(f)
+
+ruby_node = toml_data["package"]["metadata"]["ci-repos"]["ruby"]
+repo = ruby_node["repo"]
+rev = ruby_node["rev"]
+print(f"ruby_repo={repo}")
+print(f"ruby_rev={rev}")
+
+if args.github_output is not None:
+    print(f"Writing to GITHUB_OUTPUT: {args.github_output}")
+    with open(args.github_output, "at") as f:
+        print(f"ruby_repo={repo}", file=f)
+        print(f"ruby_rev={rev}", file=f)
