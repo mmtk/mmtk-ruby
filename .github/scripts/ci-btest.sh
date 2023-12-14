7ef645b47adc0dd376059c622cb7db4a43c08256
Kunshan Wang <wks1986@gmail.com>
2023-12-14 12:43:51 +0800

    Export RUST_BACKTRACE=1 for CI tests

--- .github/scripts/ci-btest.sh
+++ .github/scripts/ci-btest.sh
@@ -2,7 +2,9 @@ set -xe
 
 source $(dirname "$0")/common.sh
 
-cd $RUBY_PATH/build
+export RUST_BACKTRACE=1
+
+cd $RUBY_BUILD_PATH
 
 echo "============ Bootstrape tests (btest) ($DEBUG_LEVEL) ============="
 make btest RUN_OPTS="--mmtk-plan=Immix" TESTOPTS="-v"
