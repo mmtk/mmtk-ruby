5712a63b408e4bf4f535519c0ebfba09aad38722
Kunshan Wang <wks1986@gmail.com>
2024-08-23 18:01:20 +0800

    Expose an API for heap traversal.
    Expose the `MMTK::enumerate_object` method for implementing heap
    traversal.  That allows the Ruby VM to enable ObjectSpace.each_object
    and TracePoint.  The test case whitelist is also updated to not exclude
    related test cases so that mmtk-core binding tests will use those test
    cases.

--- mmtk/src/api.rs
+++ mmtk/src/api.rs
@@ -374,3 +374,15 @@ pub extern "C" fn mmtk_object_reference_write_post(
         ignored_target,
     )
 }
+
+/// Enumerate objects.  This function will call `callback(object, data)` for each object. It has
+/// undefined behavior if allocation or GC happens while this function is running.
+#[no_mangle]
+pub extern "C" fn mmtk_enumerate_objects(
+    callback: extern "C" fn(ObjectReference, *mut libc::c_void),
+    data: *mut libc::c_void,
+) {
+    crate::mmtk().enumerate_objects(|object| {
+        callback(object, data);
+    })
+}
