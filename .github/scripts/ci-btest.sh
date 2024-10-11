08a7e8d28e25bfec04589ccbac9a2411e5d6e4fc
Kunshan Wang <wks1986@gmail.com>
2024-10-11 22:04:07 +0800

    Workaround btest timeout in debug mode
    Run bootstraptest/test_eval.rb with a fixed heap size.

--- .github/scripts/ci-btest.sh
+++ .github/scripts/ci-btest.sh
@@ -10,5 +10,13 @@ echo "============ Bootstrape tests (btest) ($DEBUG_LEVEL) ============="
 if test "$DEBUG_LEVEL" == "vanilla"; then
     make btest TESTOPTS="-v -j${CI_JOBS}"
 else
+    if test "$DEBUG_LEVEL" == "debug"; then
+        # One test case in this file is taking too much time to run in debug mode, resulting in timeout.
+        # It is because the default dynamic heap size does not work well with that pathological use case.
+        # We simply run it with a fixed heap size and exclude it from the rest of the btests.
+        TIMEOUT_TEST=../bootstraptest/test_eval.rb
+        MMTK_GC_TRIGGER=FixedHeapSize:100m make btest RUN_OPTS="--mmtk-plan=$CHOSEN_PLAN" TESTOPTS="-v $TIMEOUT_TEST"
+        rm $TIMEOUT_TEST
+    fi
     make btest RUN_OPTS="--mmtk-plan=$CHOSEN_PLAN" TESTOPTS="-v -j${CI_JOBS}"
 fi
